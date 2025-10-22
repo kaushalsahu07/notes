7f0555678477f72bff043481f73513b9fd53e690
kaushalsahu07 <kaushalsahu.me@gmail.com>
2025-10-22 07:34:32 +0530

    feat: enhance NoteBox component with input fields and icons, update layout for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,8 @@ function App() {
     <>
     {/* Nav */}
      <Nav />
-     {/* Notes */}
-     <div className="px-8 py-4 grid grid-cols-4 gap-4"> 
-     <NoteBox />
-     <NoteBox />
-     <NoteBox />
-     <NoteBox />
-     <NoteBox />
-     <NoteBox />
+  {/* Notes - responsive grid: 1 column on xs, 2 on sm, 3 on md, 4 on lg+ */}
+  <div className="px-4 py-4 sm:px-6 md:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"> 
      <NoteBox />
      </div>
     </>
--- src/components/NoteBox.jsx
+++ src/components/NoteBox.jsx
@@ -1,17 +1,34 @@
+import { IconTrashX, IconCalendarEvent } from "@tabler/icons-react";
 import "../App.css";
 
 export default function NoteBox() {
+  const [title, setTitle] = "";
+  const [content, setContent] = "";
+
   return (
     <>
-      <div className="w-4xs h-50 bg-white border-gray-200 hover:border-blue-500 border-2 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <div className="w-4xs h-69 bg-white border-gray-200  border-2 rounded-lg shadow-md hover:shadow-lg hover:border-blue-500 transition-shadow duration-300">
         <div className="p-5">
-          <h1 className="font-bold text-[20px]">Title</h1>
-          <p className="w-2xs h-30">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-            ad distinctio, quasi assumenda obcaecati vitae? Temporibus
-            reiciendis accusantium quasi eius deserunt dolor sit amet
-            consectetur.
+          <input 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Untitled"
+          className="w-[95%] h-9 overflow-hidden font-bold text-[20px] outline-none focus:outline-none placeholder:text-gray-300" />
+          <textarea 
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Start typing your note..."
+          className="ml-2 mt-2 w-[95%] resize-none h-32 overflow-y-auto outline-none focus:outline-none placeholder:text-gray-300" />
+        </div>
+        {/* Date, delete */}
+        <div className="flex justify-between items-center px-5 pt-3">
+          <p className="text-sm flex items-center gap-1 font-bold text-gray-500">
+            <IconCalendarEvent stroke={2} className="w-5 text-blue-500" />
+            31 May, 2025
           </p>
+          <button className="text-red-500 hover:text-red-700 text-sm">
+            <IconTrashX stroke={2} className="w-5" />
+          </button>
         </div>
       </div>
     </>
